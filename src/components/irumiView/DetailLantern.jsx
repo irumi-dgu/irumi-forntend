b397d37a00a87653010cea5cfca534695df8e3ac
심서현 <simseohyun@simseohyeon-ui-MacBookAir.local>
2023-09-21 01:12:19 +0900

    :sparkles: Feat: 연등 둘러보기 오브제 배치
    오브제배치했습니다.

--- src/components/irumiView/DetailLantern.jsx
+++ src/components/irumiView/DetailLantern.jsx
@@ -1,94 +1,87 @@
 // DetailLantern.jsx
 
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import * as S from "./style";
-import MoreModal from './MoreModal';
-import DeleteModal from './DeleteModal';
+import MoreModal from "./MoreModal";
+import DeleteModal from "./DeleteModal";
 
 function DetailLantern() {
-    const [modalOpen, setModalOpen] = useState(false);
-    const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
 
-    const openModal = () => {
-        setModalOpen(true);
-    };
-    const closeModal = () => {
-        setModalOpen(false);
-    };
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
-    const openDeleteModal = () => {
-        setDeleteModalOpen(true);
-        closeModal();
-    };
-    const closeDeleteModal = () => {
-        setDeleteModalOpen(false);
-        closeModal();
-    };
+  const openDeleteModal = () => {
+    setDeleteModalOpen(true);
+    closeModal();
+  };
+  const closeDeleteModal = () => {
+    setDeleteModalOpen(false);
+    closeModal();
+  };
 
-    // get 해올거
-    const data = [{
-        "id": 3,
-        "nickname": "20 김강민",
-        "content": "여친 사귀고 싶다 여백 확인 중 길게길게 써보는 중 어케되나 함보자 배가고프구나",
-        "likes": 23,
-        "lanternColor": 5,
-        "twinkle": true,
-    }]
-
-    // 연등 -> 이미지로 매핑
-    const getImageUrl = (lanternColor, twinkle) => {
-        if (lanternColor >= 1 && lanternColor <= 5) {
-            const twinkleToggle = twinkle ? "yes" : "no";
-            return `/detail_${lanternColor}_${twinkleToggle}.svg`;
-        }
-        return "/detail_1_yes.svg";
+  // get 해올거
+  const data = [
+    {
+      id: 3,
+      nickname: "20김강민",
+      content:
+        "여친 사귀고 싶다 여백 확인 중 길게길게 써보는 중 어케되나 함보자 배가고프구나",
+      likes: 23,
+      lanternColor: 5,
+      twinkle: true
     }
+  ];
 
-    return (
-        <>
-            {data.map((item) => (
-                <S.DetailLanternWrapper key={item.id}>
-                    <img src={getImageUrl(item.lanternColor, item.twinkle)} />
-                    <S.TitleSec>{item.nickname}</S.TitleSec>
-                    <S.ContentSec>{item.content}</S.ContentSec>
-                    <S.MoreSec
-                        src="/moreBtn.png"
-                        onClick={openModal}
-                    />
-                    <S.LikeBtn>
-                        <img src="/detail_like.png" />
-                        <p>{item.likes}</p>
-                    </S.LikeBtn>
-                </S.DetailLanternWrapper>
-            ))}
+  // 연등 -> 이미지로 매핑
+  const getImageUrl = (lanternColor, twinkle) => {
+    if (lanternColor >= 1 && lanternColor <= 5) {
+      const twinkleToggle = twinkle ? "yes" : "no";
+      return `/detail_${lanternColor}_${twinkleToggle}.svg`;
+    }
+    return "/detail_1_yes.svg";
+  };
 
-            {modalOpen && (
-                <MoreModal
-                    closeModal={closeModal}
-                    openDeleteModal={openDeleteModal}
-                />
-            )}
+  return (
+    <>
+      {data.map(item => (
+        <S.DetailLanternWrapper key={item.id}>
+          <img src={getImageUrl(item.lanternColor, item.twinkle)} />
+          <S.TitleSec>{item.nickname}</S.TitleSec>
+          <S.ContentSec>{item.content}</S.ContentSec>
+          <S.MoreSec src="/moreBtn.png" onClick={openModal} />
+          <S.LikeBtn>
+            <img src="/detail_like.png" />
+            <p>{item.likes}</p>
+          </S.LikeBtn>
+        </S.DetailLanternWrapper>
+      ))}
 
-            {deleteModalOpen && (
-                <DeleteModal
-                    closeDeleteModal={closeDeleteModal}
-                />
-            )}
+      {modalOpen && (
+        <MoreModal closeModal={closeModal} openDeleteModal={openDeleteModal} />
+      )}
 
-            {deleteModalOpen && (
-                <div
-                    style={{
-                        position: 'fixed',
-                        top: 0,
-                        left: 0,
-                        width: '100%',
-                        height: '100%',
-                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                    }}
-                />
-            )}
-        </>
-    )
+      {deleteModalOpen && <DeleteModal closeDeleteModal={closeDeleteModal} />}
+
+      {deleteModalOpen && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.5)"
+          }}
+        />
+      )}
+    </>
+  );
 }
 
-export default DetailLantern
+export default DetailLantern;
--- src/pages/lanterns/Lanterns.jsx
+++ src/pages/lanterns/Lanterns.jsx
@@ -2,7 +2,63 @@ import React from "react";
 import * as S from "./style";
 
 function Lanterns() {
-  return <S.LanternsWrapper>심서현 랜턴용</S.LanternsWrapper>;
+  // get 해올거
+  const data = [
+    {
+      id: 3,
+      nickname: "일이삼사오육칠팔",
+      content:
+        "여친 사귀고 싶다 여백 확인 중 길게길게 써보는 중 어케되나 함보자 배가고프구나",
+      likes: 23,
+      lanternColor: 5,
+      twinkle: false
+    },
+    {
+      id: 2,
+      nickname: "20_김강민",
+      content:
+        "여친 사귀고 싶다 여백 확인 중 길게길게 써보는 중 어케되나 함보자 배가고프구나",
+      likes: 23,
+      lanternColor: 4,
+      twinkle: true
+    },
+    {
+      id: 2,
+      nickname: "멀티_서현",
+      content: "개발 빨리 끝났으면 좋겠습니다",
+      likes: 23,
+      lanternColor: 2,
+      twinkle: true
+    }
+  ];
+
+  const getImageUrl = (lanternColor, twinkle) => {
+    if (lanternColor >= 1 && lanternColor <= 5) {
+      const twinkleToggle = twinkle ? "yes" : "no";
+      return `/lanterns_${lanternColor}_${twinkleToggle}.svg`;
+    }
+    return "/lanterns_1_yes.svg";
+  };
+
+  return (
+    <S.LanternsWrapper>
+      <S.LanternsList>
+        {data.map(item => (
+          <S.LanternBox key={item.id}>
+            <S.Lantern>
+              <S.TitleSec>{item.nickname}</S.TitleSec>
+              <S.ContentSec>{item.content}</S.ContentSec>
+              <S.LikeBtn>
+                <img src="/detail_like_fill.png" />
+                <p>{item.likes}</p>
+              </S.LikeBtn>
+              <img src={getImageUrl(item.lanternColor, item.twinkle)} />
+            </S.Lantern>
+          </S.LanternBox>
+        ))}
+      </S.LanternsList>
+    </S.LanternsWrapper>
+  );
 }
 
 export default Lanterns;
