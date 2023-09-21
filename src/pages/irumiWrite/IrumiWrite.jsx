6c91ccadfb057ef6a292d0c1d2c82ab7bb5d36ec
Ohchanju <ohchanju3@naver.com>
2023-09-21 20:59:45 +0900

    :sparkles: Feat: write의 닉네임, 소원쓰기 작성 폼 생성

--- src/pages/irumiWrite/IrumiWrite.jsx
+++ src/pages/irumiWrite/IrumiWrite.jsx
@@ -1,5 +1,3 @@
-// IrumiWrite.jsx
-
 import React, { useState } from "react";
 import * as S from "./style";
 import Back from "../../../public/back.png";
@@ -7,14 +5,51 @@ import LanternChoice from "../../components/irumiWrite/lanternColorChoice";
 
 function IrumiWrite() {
   const [selectedColor, setSelectedColor] = useState("pink");
+  const [userWish, setUserWish] = useState("");
+  const [userWishContent, setUserWishContent] = useState(""); // 사용자의 소원을 저장할 상태 변수
+
+  // 사용자가 입력한 닉네임 관련 함수
+  const handleUserWishChange = event => {
+    const content = event.target.value;
+    // 공백 제거
+    const trimmedContent = content.replace(/\s/g, "");
+    if (trimmedContent.length <= 10) {
+      setUserWish(trimmedContent);
+    }
+  };
+
+  //사용자가 입력한 소원 내용 관련 함수
+  const handleUserWishContent = event => {
+    const content = event.target.value;
+    // 글자수가 100자를 초과하지 않도록 제한
+    if (content.length <= 100) {
+      setUserWishContent(content);
+    }
+  };
 
   return (
     <S.IrumiWriteWrapper>
       <S.backStyledImage src={Back} alt="이전" />
-      {/* LanternChoice 컴포넌트에 setSelectedColor를 전달합니다. */}
       <LanternChoice setSelectedColor={setSelectedColor} />
-      {/* selectedColor에 따라 이미지 설정 */}
-      <S.wishBgImg img src={`/write_${selectedColor}.png`} />
+      <S.wishContent>
+        <S.wishBgImg img src={`/write_${selectedColor}.png`} />
+        {/* 이미지 위에 텍스트 입력란 */}
+        <S.Textarea>
+          <S.TextareaName>닉네임</S.TextareaName>
+
+          <S.NameInput
+            value={userWish}
+            onChange={handleUserWishChange}
+            placeholder="닉네임을 입력하세요.(최대 10자, 공백 사용 불가)"
+          />
+          <S.TextareaContent>소원 내용</S.TextareaContent>
+          <S.ContentInput
+            value={userWishContent}
+            onChange={handleUserWishContent}
+            placeholder="소원 내용을 입력해주세요.(100자 이내)"
+          />
+        </S.Textarea>
+      </S.wishContent>
     </S.IrumiWriteWrapper>
   );
 }
--- src/pages/irumiWrite/style.jsx
+++ src/pages/irumiWrite/style.jsx
@@ -1,8 +1,11 @@
 import { styled } from "styled-components";
 
 export const IrumiWriteWrapper = styled.div`
-  flex-grow: 1;
+  position: fixed;
   width: 100%;
+  max-width: 420px;
+  height: 100vh;
+  flex-grow: 1;
   display: flex;
   align-items: center;
   background-image: url("background_write.png");
@@ -23,6 +26,71 @@ export const backStyledImage = styled.img`
 `;
 
 export const wishBgImg = styled.img`
-  width: 55%;
+  width: 65%;
   margin-top: 10%;
+`;
+
+export const wishContent = styled.div`
+  justify-content: center;
+  display: flex;
+
+  position: relative;
+`;
+
+export const WriteName = styled.div`
+  z-index: 999;
+`;
+
+export const Textarea = styled.div`
+  position: absolute;
+  top: 260px;
+  width: 180px;
+  display: flex;
+  flex-direction: column;
+  font-size: 16px;
+`;
+
+export const TextareaName = styled.div`
+  color: black;
+  font-size: 0.3rem;
+`;
+
+export const NameInput = styled.input`
+  border: 1px solid #898989;
+  margin-top: 7px;
+  background-color: whitesmoke;
+  border-radius: 5px;
+  height: 24px;
+  padding: 7px;
+
+  &::placeholder {
+    font-size: 5px;
+  }
+`;
+
+export const TextareaContent = styled.div`
+  color: black;
+  font-size: 0.3rem;
+  margin-top: 13px;
+`;
+
+export const ContentInput = styled.textarea`
+  border: 1px solid #898989;
+  margin-top: 7px;
+  background-color: whitesmoke;
+  border-radius: 5px;
+  height: 190px;
+  padding: 7px;
+  width: 100%;
+  justify-content: center;
+  display: flex;
+  font-family: "DonggukFont";
+
+  &::placeholder {
+    font-size: 10px; /* placeholder 텍스트 크기 설정 */
+  }
+
+  &:focus {
+    border-color: #898989;
+  }
 `;
