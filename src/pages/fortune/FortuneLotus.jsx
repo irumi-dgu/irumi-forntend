342d45c2a3a4763c357daaf62fa72d643c4a1238
kickcha <chasm030517@gmail.com>
2023-09-21 10:30:12 +0900

    ✨Feat:포춘쿠키 시작

--- src/pages/fortune/FortuneLotus.jsx
+++ src/pages/fortune/FortuneLotus.jsx
@@ -6,7 +6,10 @@ function FortuneLotus() {
   return (
     <S.StarWrapper>
       <S.SparkleWrapper>
-        <S.LotusLeaf />
+        <S.LotusLeaf>
+          <p>악으로 깡으로 버텨보세요</p>
+        </S.LotusLeaf>
+        <S.ClosedButton />
       </S.SparkleWrapper>
     </S.StarWrapper>
   );
--- src/pages/fortune/style.jsx
+++ src/pages/fortune/style.jsx
@@ -1,5 +1,6 @@
 import { styled } from "styled-components";
 
+//별만 있는 배경
 export const StarWrapper = styled.div`
   flex-grow: 1;
   width: 100%;
@@ -12,6 +13,7 @@ export const StarWrapper = styled.div`
   background-position: center;
 `;
 
+//반짝이 배경 - 모션 추가 예정
 export const SparkleWrapper = styled.div`
   flex-grow: 1;
   width: 100%;
@@ -26,11 +28,15 @@ export const SparkleWrapper = styled.div`
 
 export const LotusWhole = styled.div`
   flex-grow: 1;
-  background-image: url("/lotus.png");
+  display: flex;
+  background-image: url(/lotus.png);
+  position: relative;
   width: 100%;
+  justify-content: center;
+  align-items: center;
   background-size: contain;
-  background-position: center;
   background-repeat: no-repeat;
+  background-position: center;
 `;
 
 export const LotusTop = styled.div`
@@ -38,9 +44,11 @@ export const LotusTop = styled.div`
   z-index: 10;
   background-image: url(/lotus2.png);
   position: relative;
-  top: 456px;
+  top: 161px;
+  width: 332px;
   height: 195px;
   opacity: 0;
+  justify-content: center;
   background-size: contain;
   background-position: center;
   background-repeat: no-repeat;
@@ -55,14 +63,39 @@ export const FortuneTitle = styled.div`
   color: #ffffff;
 `;
 
-//포춘 연꽃잎 - 부모요소 때문에 크기 지정이 안되는듯 오늘 수정할게요
+//포춘 연꽃잎
 export const LotusLeaf = styled.div`
   flex-grow: 1;
   border: 3px solid red;
   background-image: url(/leaf.png);
-  position: relative;
-  width: 67%;
+  position: absolute;
+  width: 30%;
+  height: 63%;
+  top: 35px;
+  margin: 5rem;
   background-size: contain;
   background-position: center;
   background-repeat: no-repeat;
+  p {
+    writing-mode: vertical-rl; /* 텍스트의 쓰기 모드를 세로로 (우에서 좌로) 설정 */
+    text-orientation: mixed; /* 텍스트 방향 설정 */
+    white-space: nowrap;
+    color: #5f3a16;
+    font-size: 16px;
+    text-align: center; /* 텍스트를 수직으로 중앙에 정렬 */
+    line-height: 100%; /* 텍스트의 라인 높이를 조절하여 중앙에 정렬 */
+  }
+`;
+
+export const ClosedButton = styled.div`
+  display: flex;
+  background-image: url(/closedbutton.png);
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 46px;
+  height: 46px;
+  top: 336px;
+  position: relative;
+  /* border: 3px solid yellow; */
 `;
