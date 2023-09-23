2d5c58093f60ff6ff320a3e12bcb7767b3fd2248
kickcha <chasm030517@gmail.com>
2023-09-23 19:50:13 +0900

    💡Feat:MyDetail 디테일렌턴 주석처리

--- src/pages/fortune/MyDetail.jsx
+++ src/pages/fortune/MyDetail.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import * as S from "./style";
-import DetailLantern from "../../components/irumiView/detailLantern";
+// import DetailLantern from "../../components/irumiView/detailLantern";
 
 function MyDetail() {
-  return (
-    <S.StarWrapper>
-      <DetailLantern />
-    </S.StarWrapper>
-  );
+  return <S.StarWrapper>{/* <DetailLantern /> */}</S.StarWrapper>;
 }
 
 export default MyDetail;
