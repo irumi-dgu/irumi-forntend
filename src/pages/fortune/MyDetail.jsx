14e09108a07397092b59423460ea69eddbe7b776
kickcha <chasm030517@gmail.com>
2023-09-24 06:37:45 +0900

    netlify 실패로 재업

--- src/pages/fortune/MyDetail.jsx
+++ src/pages/fortune/MyDetail.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import * as S from "./style";
-import DetailLantern from "../../components/irumiView/detailLantern";
 
 function MyDetail() {
   return (
     <S.IrumiViewWrapper>
-      <DetailLantern />
+      <h1>DeatilLantern을 불러오는 순간 배포가 안되네용..</h1>
+      <h2>
+        근데 이건 디테일이랑도 조금 다른 로직인 것 같아서 다시 생각을 해봐야할
+        것 같습니다 + 우리 이미지 저장기능도 해내야함..
+      </h2>
     </S.IrumiViewWrapper>
   );
 }
