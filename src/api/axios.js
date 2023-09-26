b9b90bedcc9519528e9f17f947bcb0cdfc933487
kickcha <chasm030517@gmail.com>
2023-09-27 00:27:07 +0900

    baseURL 수정, env 추가

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "",
+  baseURL: process.env.REACT_APP_API_URL, // 올바른 구문
   headers: {
     "Content-Type": "application/json"
   }
