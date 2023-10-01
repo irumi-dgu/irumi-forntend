78fbae6803f2944a7c5eb9fcfb6c87af9cec3c6d
kickcha <chasm030517@gmail.com>
2023-10-02 05:02:17 +0900

    baseURL 수정 - https

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,10 +2,11 @@ import axios from "axios";
 
 export const API = axios.create({
   // baseURL: "http://127.0.0.1:8000",
-  baseURL: "http://iirumi.com/",
+  baseURL: "https://iirumi.com/",
   //https + http localhost 조합 = 실패
   //http + http localhost 조합 = 보류 (서버는 열리는데 통신이 안됨)
-  //http + http iirumi.com 조합 =
+  //http + http iirumi.com 조합 = 쓰레기
+  //https + http iirumi.com 조합
   headers: {
     "Content-Type": "application/json"
   }
