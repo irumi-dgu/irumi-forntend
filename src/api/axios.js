d8973c726b365643717ee641d4e9408f865afa3d
kickcha <chasm030517@gmail.com>
2023-10-02 03:37:41 +0900

    🔥Feat: baseURL 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const API = axios.create({
   // baseURL: "http://127.0.0.1:8000",
-  baseURL: "http://iirumi.com/",
+  baseURL: "https://iirumi.com/",
   headers: {
     "Content-Type": "application/json"
   }
