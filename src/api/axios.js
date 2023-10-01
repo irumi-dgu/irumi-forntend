10154ad8209d418b30303e24fe0aa1d4ec9f25ff
kickcha <chasm030517@gmail.com>
2023-10-02 05:00:20 +0900

    congif api 수정 - iirumi.com

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,8 @@ export const API = axios.create({
   // baseURL: "http://127.0.0.1:8000",
   baseURL: "http://iirumi.com/",
   //https + http localhost 조합 = 실패
+  //http + http localhost 조합 = 보류 (서버는 열리는데 통신이 안됨)
+  //http + http iirumi.com 조합 =
   headers: {
     "Content-Type": "application/json"
   }
