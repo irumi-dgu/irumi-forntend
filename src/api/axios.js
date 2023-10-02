3b02b031a1c72fde6b128e4faf25fc8c1d416990
kickcha <chasm030517@gmail.com>
2023-10-02 15:00:24 +0900

    package.json start script 수정, config https

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,10 +4,12 @@ export const API = axios.create({
   // baseURL: "http://127.0.0.1:8000",
   baseURL: "https://iirumi.com/",
   //axios.js + config.js
-  //https + http localhost 조합 = 실패
-  //http + http localhost 조합 = 보류 (서버는 열리는데 통신이 안됨)
+  //http + http localhost 조합 = 보류 (통신 안됨)
   //http + http iirumi.com 조합 = 쓰레기
-  //https + http iirumi.com 조합 = 성공 (일단은 되긴 됨 서버도 열리고 통신도 됨)
+
+  //https + http iirumi.com 조합 = 성공 (일단 되긴 됨 서버도 열리고 통신도 됨) 다음날 접속 X
+  //https + https iirumi.com 조합 = 보류 (통신 안됨) 재시도
+  //https + http localhost 조합 = 실패
   headers: {
     "Content-Type": "application/json"
   }
