381c5af3eeda040d47daf9ede575abd92dbbceb6
kickcha <chasm030517@gmail.com>
2023-10-02 14:19:15 +0900

    config 수정 - https

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,10 +3,11 @@ import axios from "axios";
 export const API = axios.create({
   // baseURL: "http://127.0.0.1:8000",
   baseURL: "https://iirumi.com/",
+  //axios.js + config.js
   //https + http localhost 조합 = 실패
   //http + http localhost 조합 = 보류 (서버는 열리는데 통신이 안됨)
   //http + http iirumi.com 조합 = 쓰레기
-  //https + http iirumi.com 조합
+  //https + http iirumi.com 조합 = 성공 (일단은 되긴 됨 서버도 열리고 통신도 됨)
   headers: {
     "Content-Type": "application/json"
   }
