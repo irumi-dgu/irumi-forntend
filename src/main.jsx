54ceec47bfc34dd42422e7c33ded55ff4a112f46
Ohchanju <ohchanju3@naver.com>
2023-10-07 03:39:04 +0900

    :sparkles: Feat: GA setting

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import ReactGA from "react-ga";
+
+ReactGA.initialize("G-YQYNTR2KWS");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
