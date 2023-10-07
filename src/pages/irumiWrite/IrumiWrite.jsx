6aa7b85843169ae272710cf7304401d7e96a948d
Ohchanju <ohchanju3@naver.com>
2023-10-08 00:22:03 +0900

    :sparkles: Feat: GA reSetting

--- src/pages/irumiWrite/IrumiWrite.jsx
+++ src/pages/irumiWrite/IrumiWrite.jsx
@@ -57,12 +57,12 @@ function IrumiWrite() {
   // 색상 선택 핸들러
   const handleColorChange = color => {
     setSelectedColor(color); // 선택한 숫자로 업데이트
-    // trackColorSelection(color);
+    trackColorSelection(color);
   };
 
-  // const trackColorSelection = color => {
-  //   gtag("event", "lanter_color", { lanter_color: color });
-  // };
+  const trackColorSelection = color => {
+    gtag("event", "lanter_color", { lanter_color: color });
+  };
 
   // '작성하기' 버튼의 스타일 업데이트 함수
   const updateSubmitButtonStyle = () => {
