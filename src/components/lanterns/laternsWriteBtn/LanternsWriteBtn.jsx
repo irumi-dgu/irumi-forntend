ad85f448dae1be5d6ca36545b0813b41849ff021
Ohchanju <ohchanju3@naver.com>
2023-10-08 02:46:26 +0900

    :sparkles: Feat: GA 설정

--- src/components/lanterns/laternsWriteBtn/LanternsWriteBtn.jsx
+++ src/components/lanterns/laternsWriteBtn/LanternsWriteBtn.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import * as S from "./style";
 import { Link } from "react-router-dom";
 
+function trackClick() {
+  gtag("event", "Lanterns_write", {
+    event_category: "Lanterns_write"
+  });
+}
+
 function LanternsWriteBtn() {
   return (
     <>
       <S.WriteBtnLink to="/irumiWrite">
-        <S.WriteBtn>
+        <S.WriteBtn onClick={trackClick}>
           <img src="/icon_pen.svg" style={{ width: "40px" }} />
         </S.WriteBtn>
       </S.WriteBtnLink>
