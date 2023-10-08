896a36ae9ebbb2da34cdf9c4ab47d1ad64f31eb2
Ohchanju <ohchanju3@naver.com>
2023-10-08 14:26:33 +0900

    :sparkles: Feat: 데이터 로딩 시 로딩중 글씨

--- src/pages/irumiView/Irumi.jsx
+++ src/pages/irumiView/Irumi.jsx
@@ -9,11 +9,11 @@ import MyDetailBackBtn from "../../components/common/backBtn/LanternsBackBtn";
 function IrumiView() {
   const { detailId } = useParams();
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
   // 내용
-  const [lanternDetail, setLanternDetail] = useState(null); // 초기값을 null로 설정
+  const [lanternDetail, setLanternDetail] = useState(null);
   const [isLiked, setIsLiked] = useState(false);
   const [likeCount, setLikeCount] = useState(false);
-
   const [lightBool, setLightBool] = useState(false);
 
   // get 해올거
@@ -27,10 +27,15 @@ function IrumiView() {
       setLikeCount(response.data.like_cnt);
       setLightBool(response.data.light_bool);
     } catch (error) {
+      // 데이터 로딩 실패 시 에러 처리
       // console.log("각 id에 해당하는 연등 디테일 가져오는 중 에러 발생", error);
-      setLanternDetail(null); // 에러가 발생한 경우 데이터를 null로 설정
+      setLanternDetail(null);
+    } finally {
+      // 데이터 로딩이 끝났음을 나타내는 상태 업데이트
+      setIsLoading(false);
     }
   };
+
   useEffect(() => {
     fetchLanternDetailData();
   }, [detailId]);
@@ -42,25 +47,35 @@ function IrumiView() {
           <MyDetailBackBtn />
         </S.Header>
 
-        {lanternDetail === null ? ( // 데이터가 null인 경우에만 텍스트를 렌더링
-          <>
-            <S.TextWrapper>
-              <S.FixText>존재하지 않는 연등이에요</S.FixText>
-            </S.TextWrapper>
-          </>
+        {isLoading ? ( // 데이터 로딩 중인지 확인
+          <S.TextWrapper>
+            <S.FixText>
+              로딩 중 !
+              <br /> 잠시만 기다려주세요 :)
+            </S.FixText>
+          </S.TextWrapper>
         ) : (
-          [lanternDetail].map(item => (
-            <DetailLantern
-              key={item.id}
-              data={item}
-              isLiked={isLiked}
-              setIsLiked={setIsLiked}
-              likeCount={likeCount}
-              setLikeCount={setLikeCount}
-              lightBool={lightBool}
-              setLightBool={setLightBool}
-            />
-          ))
+          // 데이터 로딩이 완료된 경우에만 연등 정보를 렌더링
+          <>
+            {lanternDetail === null ? (
+              <S.TextWrapper>
+                <S.FixText>존재하지 않는 연등이에요</S.FixText>
+              </S.TextWrapper>
+            ) : (
+              [lanternDetail].map(item => (
+                <DetailLantern
+                  key={item.id}
+                  data={item}
+                  isLiked={isLiked}
+                  setIsLiked={setIsLiked}
+                  likeCount={likeCount}
+                  setLikeCount={setLikeCount}
+                  lightBool={lightBool}
+                  setLightBool={setLightBool}
+                />
+              ))
+            )}
+          </>
         )}
       </S.IrumiViewWrapper>
     </>
