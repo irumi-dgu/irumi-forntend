58890a4a6335eed49f090aa821133a64a79bf292
kickcha <chasm030517@gmail.com>
2023-10-09 21:14:50 +0900

    디테일 백버튼 수정

--- src/components/common/backBtn/MyDetailBackBtn.jsx
+++ src/components/common/backBtn/MyDetailBackBtn.jsx
@@ -9,25 +9,22 @@ export const BackBtnBox = styled.div`
   justify-content: center;
   align-items: center;
   cursor: pointer;
+  z-index: 1000;
 `;
 
 const MyDetailBackBtn = () => {
   const navigate = useNavigate();
   const handleBackClick = () => {
-    // 이전 페이지가 없는 경우 홈 페이지로 이동
-    if (document.referrer) {
-      navigate(-1);
-    } else {
-      navigate("/lanterns");
-    }
-    // if (location.state && location.state.from) {
-    //   console.log(location.state);
-    //   console.log(location.state.from);
-    //   navigate(location.state.from);
-    // } else {
-    //   navigate("/");
-    // }
+    navigate("/lanterns");
   };
+  // if (location.state && location.state.from) {
+  //   console.log(location.state);
+  //   console.log(location.state.from);
+  //   navigate(location.state.from);
+  // } else {
+  //   navigate("/");
+  // }
+
   return (
     <BackBtnBox onClick={handleBackClick}>
       <img src="/backBtn.png" style={{ width: "20px" }} />
